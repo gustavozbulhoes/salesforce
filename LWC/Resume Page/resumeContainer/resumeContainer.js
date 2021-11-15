423c9e20ba203d9e836e70db94529f4ce9fff369
Gustavo Zanatta Bulhões <61094344+Gustavozbulhoess@users.noreply.github.com>
2021-11-15 00:37:26 -0300

    Update resumeContainer.js

--- LWC/Resume Page/resumeContainer/resumeContainer.js
+++ LWC/Resume Page/resumeContainer/resumeContainer.js
@@ -9,4 +9,8 @@ import * as RESUME_DATA from './resumeContainerData'
 
 export default class ResumeContainer extends LightningElement {
     RESUME_DATA = RESUME_DATA
+    
+    connectedCallback(){
+        console.log("Author:","Gustavo Zanatta Bulhões")
+    }
 }
